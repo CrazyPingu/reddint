f4aac4690f6566404d28b72bcfecb642916af18c
Samuele De Tuglie <samuele.de.tuglie@gmail.com>
2022-12-23 19:34:33 +0100

    second commit of base.php

--- site/template/base.php
+++ site/template/base.php
@@ -1,15 +1,37 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang='en'>
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <link rel='stylesheet' type="text/css" href='../css/style.css'>
+    <link rel="icon" href="../res/favicon.png" type="image/png" sizes="16x16">
+    <title><?php echo $templateParams['title']; ?></title>
 </head>
 
 <body>
-
+    <header>
+        <h1>Reddint</h1>
+    </header>
+    <div class="navbar">
+        <ul>
+            <li><a href="profile.php">Profile</a></li>
+            <li><a href="notification.php">Notification</a></li>
+            <li><a href="settings.php">Settings</a></li>
+        </ul>
+        <form method="POST">
+            <input type="text" name="search" placeholder="Search">
+            <button type="submit">Search</button>
+        </form>
+    </div>
+    <main>
+        <?php
+        if (isset($templateParams['name'])) {
+            require($templateParams['name']);
+        }
+        ?>
+    </main>
+    <script src="<?php $templateParams['script'] ?? '';?>"></script>
 </body>
 
 </html>
