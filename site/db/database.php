c44d5bc067de7d0f70fab4e948851f9a1c676cd9
Emanuele Borghini <manuborg2002@gmail.com>
2022-12-23 19:39:54 +0100

    Add getRandomPosts to database helper class

--- site/db/database.php
+++ site/db/database.php
@@ -9,6 +9,19 @@ class DatabaseHelper{
             die("Connection failed: " . $this->db->connect_error);
         }
     }
+
+    public function __destruct(){
+        $this->db->close();
+    }
+
+    public function getRandomPosts(int $limit = 1){
+        $sql = "SELECT * FROM posts ORDER BY RAND() LIMIT ?";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bind_param("i", $limit);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
 }
 
 ?>
