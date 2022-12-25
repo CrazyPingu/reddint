b2a932fa2fb082cc036c6edd53cef7ae7614575f
Emanuele Borghini <manuborg2002@gmail.com>
2022-12-25 18:12:09 +0100

    fix login check

--- site/pre-checks.php
+++ site/pre-checks.php
@@ -1,6 +1,6 @@
 <?php
 
-if(isset($_SESSION["userId"]) && isset($_SESSION["username"])) {
+if(!isset($_SESSION["userId"]) || !isset($_SESSION["username"])) {
     header("Location: index.php");
 }
 
