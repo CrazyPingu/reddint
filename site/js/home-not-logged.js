e16f89cc7ec9216d957091db940410baf16bdec4
Cristina Zoccola <cristina.zoccola01@gmail.com>
2022-12-26 23:13:14 +0100

    Added onload function

--- site/js/home-not-logged.js
+++ site/js/home-not-logged.js
@@ -1,1 +1,8 @@
 import asyncCall from "./default-ajax.js";
+
+//show initial posts
+window.onload = function() {
+    asyncCall('random-posts.php', (response) => {
+        spacePosts.innerHTML = response;
+    });
+}
