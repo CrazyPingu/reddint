53abc543ff100fa376f9cef584bfcd4bb7ec78a2
Cristina Zoccola <cristina.zoccola01@gmail.com>
2022-12-26 23:18:39 +0100

    Added scroll listener

--- site/js/home-logged.js
+++ site/js/home-logged.js
@@ -8,6 +8,14 @@ window.onload = () => {
 const buttonUsers = document.querySelector('#buttonPostUsers');
 const buttonCommunities = document.querySelector('#buttonPostCommunities');
 
+//the div where the posts will be shown
+const spacePosts = document.querySelector('.space');
+
+//event listeners on click and scroll
+let lastSelected = '';
+const baseOffset = 10;
+let offset = 0;
+let page = '';
 
 buttonUsers.addEventListener('click', () => {
     if (lastSelected === 'users') return;
@@ -25,5 +33,15 @@ buttonCommunities.addEventListener('click', () => {
         }, 'offset=0')
     offset = baseOffset;
     lastSelected = 'communities';
+});
+
+spacePosts.addEventListener('scroll', () => {
+    if (spacePosts.scrollTop >= (spacePosts.scrollHeight - spacePosts.offsetHeight)) {
+        lastSelected === 'users' ? page='home-users-posts.php' : page='home-communities-posts.php';
+        asyncCall(page, (response) => {
+            spacePosts.innerHTML += response;
+        }, 'offset=' + offset)
+    }
+    offset += baseOffset;
 });
 
