193fbb2f82284f0502d7f7cc30e3f6c22a46167c
Cristina Zoccola <cristina.zoccola01@gmail.com>
2022-12-26 23:13:44 +0100

    Added click listener to the buttons

--- site/js/home-not-logged.js
+++ site/js/home-not-logged.js
@@ -1,5 +1,8 @@
 import asyncCall from "./default-ajax.js";
 
+//the buttons need to redirect to the login page
+const buttons = document.querySelectorAll('.buttons');
+buttons.forEach((button) => button.addEventListener('click', () => window.location.href = './login.php'));
 //show initial posts
 window.onload = function() {
     asyncCall('random-posts.php', (response) => {
