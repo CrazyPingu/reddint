9d29b6b10a00f4dfa5dcc290adf657e502f4e7fe
Cristina Zoccola <cristina.zoccola01@gmail.com>
2022-12-26 23:14:16 +0100

    added scroll listener

--- site/js/home-not-logged.js
+++ site/js/home-not-logged.js
@@ -3,9 +3,22 @@ import asyncCall from "./default-ajax.js";
 //the buttons need to redirect to the login page
 const buttons = document.querySelectorAll('.buttons');
 buttons.forEach((button) => button.addEventListener('click', () => window.location.href = './login.php'));
+
+//the div where the posts will be shown
+const spacePosts = document.querySelector('.space');
+
 //show initial posts
 window.onload = function() {
     asyncCall('random-posts.php', (response) => {
         spacePosts.innerHTML = response;
     });
 }
+
+//added listener to show new posts when reached the bottom
+spacePosts.addEventListener('scroll', () => {
+    if(spacePosts.scrollTop >= (spacePosts.scrollHeight - spacePosts.offsetHeight)) {
+        asyncCall('random-posts.php', (response) => {
+            spacePosts.innerHTML += response;
+        });
+    }
+});
