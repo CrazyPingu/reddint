d49a6ec3da06eab5f781df3b627f7afed179868e
Cristina Zoccola <cristina.zoccola01@gmail.com>
2022-12-26 23:17:25 +0100

    Added click listener

--- site/js/home-logged.js
+++ site/js/home-logged.js
@@ -4,3 +4,26 @@ window.onload = () => {
     buttonUsers.click();
 }
 
+//the buttons
+const buttonUsers = document.querySelector('#buttonPostUsers');
+const buttonCommunities = document.querySelector('#buttonPostCommunities');
+
+
+buttonUsers.addEventListener('click', () => {
+    if (lastSelected === 'users') return;
+    asyncCall('home-users-posts.php', (response) => {
+        spacePosts.innerHTML = response;
+    }, 'offset=0')
+    offset = baseOffset;
+    lastSelected = 'users';
+});
+
+buttonCommunities.addEventListener('click', () => {
+    if (lastSelected === 'communities') return;
+        asyncCall('home-communities-posts.php', (response) => {
+            spacePosts.innerHTML = response;
+        }, 'offset=0')
+    offset = baseOffset;
+    lastSelected = 'communities';
+});
+
