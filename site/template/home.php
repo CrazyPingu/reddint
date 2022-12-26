1afdbcec58f07c33e588ebc7dc5441bcde3cacd8
Cristina Zoccola <cristina.zoccola01@gmail.com>
2022-12-26 23:03:11 +0100

    Created the template for the home of the site

---
+++ site/template/home.php
@@ -0,0 +1,5 @@
+<div class='buttons'>
+    <button type='button' id='buttonPostUsers'>Users</button>
+    <button type='button' id='buttonPostCommunities'>Communities</button>
+</div>
+<div class='space'></div>
