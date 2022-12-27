2dc8b46523105bf755b5a06884bde675bdc55cbb
Cristina Zoccola <cristina.zoccola01@gmail.com>
2022-12-27 23:25:22 +0100

    Used new functions

--- site/requests/home-communities-posts.php
+++ site/requests/home-communities-posts.php
@@ -2,8 +2,8 @@
 require_once '../bootstrap.php';
 include_once 'generate-posts.php';
 
-$communitiesFollowed; //= metodo da implementare
-$posts = $dbh->getPostsByCommunities(/*da cambiare*/$communitiesFollowed, 10, $_POST['offset']);
+$communitiesFollowed = getParticipatingCommunities($_SESSION['userId']);
+$posts = $dbh->getPostsByCommunities(array_column($communitiesFollowed, 'id'), 10, $_POST['offset'] ?? 0);
 
 echo generateJson($posts, $dbh);
 ?>
--- site/requests/home-users-posts.php
+++ site/requests/home-users-posts.php
@@ -3,7 +3,7 @@ require_once '../bootstrap.php';
 include_once 'generate-posts.php';
 
 $usersFollowed = $dbh->getFollowed($_SESSION['userId']);
-$posts = $dbh->getPostsByUsers($usersFollowed/*da cambiare*/, 10, $_POST['offset']);
+$posts = $dbh->getPostsByUsers(array_column($usersFollowed, 'id'), 10, $_POST['offset'] ?? 0);
 
 echo generateJson($posts, $dbh);
 ?>
