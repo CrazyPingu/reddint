718f4a9c0917efc7d9873ce8a390f26d03be8857
Samuele De Tuglie <samuele.de.tuglie@gmail.com>
2022-12-28 20:05:38 +0100

    Improved code readability in default ajax

--- site/js/default-ajax.js
+++ site/js/default-ajax.js
@@ -1,8 +1,5 @@
 function asyncCall(fileName, callback = () => { }, args = null, contentType = 'application/x-www-form-urlencoded') {
-    let jsonArgs = null;
-    if(args != null){
-        jsonArgs = JSON.stringify(args);
-    }
+    let jsonArgs = args ? JSON.stringify(args) : null;
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
@@ -11,7 +8,7 @@ function asyncCall(fileName, callback = () => { }, args = null, contentType = 'a
     };
     xhttp.open('POST', 'requests/' + fileName, true);
     xhttp.setRequestHeader('Content-type', contentType);
-    xhttp.send('args=' + jsonArgs);
+    xhttp.send(jsonArgs ? 'args=' + jsonArgs : '');
 }
 
 export default asyncCall;
