27d663da8d86add8d50d470d7a5127c4e1b7b23d
Emanuele Borghini <manuborg2002@gmail.com>
2022-12-28 21:57:09 +0100

    Add checks for unset variables in pre checks

--- site/pre-checks.php
+++ site/pre-checks.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION)) {
 // Get the current page that requested the pre-checks
 $page = basename($_SERVER['PHP_SELF']);
 // Check the session variables to see if the user is logged in
-$isUserLogged = $_SESSION['userId'] && $_SESSION['username'];
+$isUserLogged = isset($_SESSION['userId']) && isset($_SESSION['username']);
 
 // Redirect from login.php to index.php if the user is already logged in
 if ($isUserLogged && $page === 'login.php') {
