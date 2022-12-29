ec697d1d3668b225ee6ea70f432e8c788e9f1b12
Samuele De Tuglie <samuele.de.tuglie@gmail.com>
2022-12-29 16:18:55 +0100

    Add navbar css

--- site/template/base.php
+++ site/template/base.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang='en'>
+
 <head>
     <meta charset='UTF-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
@@ -7,19 +8,26 @@
     <link rel='icon' href='res/favicon.png' type='image/png' sizes='16x16'>
     <title><?php echo $templateParams['title']; ?></title>
 </head>
+
 <body>
     <header>
         <div class='navbar'>
-            <h1><a href='./index.php'>Reddint</a></h1>
-            <ul>
-                <li><a href=<?php echo 'profile.php' . (isset($_SESSION['username']) ? '?username=' . $_SESSION['username'] : ''); ?>>Profile</a></li>
-                <li><a href='notification.php'>Notifications</a></li>
-                <li><a href='settings.php'>Settings</a></li>
-            </ul>
-            <form action='#' method='POST'>
-                <input type='text' name='search' placeholder='Search'>
-                <button type='submit'>Search</button>
-            </form>
+            <h1><a href='./index.php' class='logo'>Reddint</a></h1>
+            <input type='checkbox' id='toggler'>
+            <label for='toggler'><i class='ri-menu-line'></i></label>
+            <div class='menu'>
+                <ul class='list'>
+                    <li>
+                        <form action='#' method='POST'>
+                            <input type='text' name='search' placeholder='Search'>
+                            <button type='submit'>Search</button>
+                        </form>
+                    </li>
+                    <li><a href=<?php echo 'profile.php' . (isset($_SESSION['username']) ? '?username=' . $_SESSION['username'] : ''); ?>>Profile</a></li>
+                    <li><a href='notification.php'>Notifications</a></li>
+                    <li><a href='settings.php'>Settings</a></li>
+                </ul>
+            </div>
         </div>
     </header>
     <main>
