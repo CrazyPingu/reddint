7476a1accf4203450f47289158dcf2effcf34688
Cristina Zoccola <cristina.zoccola01@gmail.com>
2022-12-30 22:42:09 +0100

    Added exit()

--- site/pre-checks.php
+++ site/pre-checks.php
@@ -3,6 +3,7 @@
 // Redirect to index.php if the session is not set
 if (!isset($_SESSION)) {
     header('Location: index.php');
+    exit();
 }
 
 // Get the current page that requested the pre-checks
@@ -13,7 +14,7 @@ $isUserLogged = isset($_SESSION['userId']) && isset($_SESSION['username']);
 // Redirect from login.php to index.php if the user is already logged in
 if ($isUserLogged && $page === 'login.php') {
     header('Location: index.php');
-    exit;
+    exit();
 }
 
 ?>
