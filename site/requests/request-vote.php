98534c0d1bf0b78c78e13a184b29ff114227f175
Cristina Zoccola <cristina.zoccola01@gmail.com>
2022-12-30 19:27:23 +0100

    Changed from if to switch

--- site/requests/request-vote.php
+++ site/requests/request-vote.php
@@ -8,14 +8,17 @@ if (!isset($_SESSION['userId'])) {
 
 $args = json_decode($_POST['args'], false);
 
-if ($args->type == 'post') {
-    $dbh->votePost($_SESSION['userId'], $args->id, $args->vote);
-    $vote = $dbh->getUserPostVote($_SESSION['userId'], $args->id);
-    $score = $dbh->getPostVote($args->id);
-} else if ($args->type == 'comment'){
-    $dbh->voteComment($_SESSION['userId'], $args->id, $args->vote);
-    $vote = $dbh->getUserCommentVote($_SESSION['userId'], $args->id);
-    $score = $dbh->getCommentVote($args->id);
+switch($args->type) {
+    case 'post':
+        $dbh->votePost($_SESSION['userId'], $args->id, $args->vote);
+        $vote = $dbh->getUserPostVote($_SESSION['userId'], $args->id);
+        $score = $dbh->getPostVote($args->id);
+        break;
+    case 'comment':
+        $dbh->voteComment($_SESSION['userId'], $args->id, $args->vote);
+        $vote = $dbh->getUserCommentVote($_SESSION['userId'], $args->id);
+        $score = $dbh->getCommentVote($args->id);
+        break;
 }
 
 echo json_encode(array('vote'=>$vote ?? 'error', 'score'=>$score ?? 'error'));
