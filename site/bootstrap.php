d4ad0da0918cac0a5d18164d471d4baa24f79196
Cristina Zoccola <cristina.zoccola01@gmail.com>
2022-12-31 17:14:34 +0100

    Updated db

--- site/bootstrap.php
+++ site/bootstrap.php
@@ -2,5 +2,5 @@
 session_start();
 define('RES_DIR', './res/');
 require_once 'db/database.php';
-$dbh = new DatabaseHelper('detu.ddns.net', 'user', 'password', 'reddint', 3306);
+$dbh = new DatabaseHelper('detu.ddns.net', 'reddint', 'Chridbpd31122022', 'reddint', 3306);
 ?>
