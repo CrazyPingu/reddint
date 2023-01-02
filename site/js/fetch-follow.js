151c9c9605bf615bcf2498787f88c7df07f595af
Cristina Zoccola <cristina.zoccola01@gmail.com>
2023-01-02 16:57:13 +0100

    Added followers and following generation

--- site/js/fetch-follow.js
+++ site/js/fetch-follow.js
@@ -1,4 +1,5 @@
 import asyncRequest from './default-ajax.js';
+import { generateElements } from './elementGenerators.js';
 
 function toggleFollow(button, usernameProfile) {
     button.addEventListener('click', function () {
@@ -6,8 +7,42 @@ function toggleFollow(button, usernameProfile) {
             if (response) {
                 button.innerText = button.innerText == 'Follow' ? 'Unfollow' : 'Follow';
             }
-        }, { usernameProfile });
+        }, { usernameProfile, type: 'follow' });
     });
 }
+
+window.onload = function () {
+    const usernameProfile = document.getElementsByClassName('followContainer')[0].id;
+    const listFollowTag = document.getElementsByClassName('listFollow')[0];
+    let offset = 0;
+    let baseOffset = 10;
+    const args = { type: listFollowTag.id, usernameProfile, offset, limit: baseOffset };
+
+    asyncRequest('request-follow.php', (response) => {
+        generateElements(response, listFollowTag, 'follow');
+    }, args);
+
+    document.querySelectorAll('#followers, #following').forEach(button => {
+        button.addEventListener('click', () => {
+            listFollowTag.innerHTML = '';
+            args.type = button.id == 'followers' ? 'followersList' : 'followingList';
+            asyncRequest('request-follow.php', (response) => {
+                generateElements(response, listFollowTag, 'follow');
+            }, args);
+        });
+    });
+
+    listFollowTag.addEventListener('scroll', function () {
+        if (listFollowTag.scrollTop + listFollowTag.clientHeight >= listFollowTag.scrollHeight) {
+            offset += baseOffset;
+            args.offset = offset;
+            asyncRequest('request-follow.php', (response) => {
+                generateElements(response, listFollowTag, 'follow');
+            }, args);
+        }
+    });
+}
+
+
 
 export default toggleFollow;
