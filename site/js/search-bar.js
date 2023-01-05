bef81e027d8cc3be042c94a44c500790b76596e0
Emanuele Borghini <manuborg2002@gmail.com>
2023-01-05 19:57:56 +0100

    Changed search request and ajax call to match the new search function

--- site/js/search-bar.js
+++ site/js/search-bar.js
@@ -8,21 +8,19 @@ searchBar.addEventListener('keyup', () => {
     if (searchBar.value.length > 0) {
         asyncRequest('request-search.php', (response) => {
             searchSpace.innerHTML = '';
-            response['user'].forEach(element => {
-                searchSpace.appendChild(
-                    Object.assign(document.createElement('a'),
-                        {
-                            href: `./profile.php?username=${encodeURIComponent(element['username'])}`,
-                            innerText: element['username']
-                        }));
-            });
-            response['communities'].forEach(element => {
-                searchSpace.appendChild(
-                    Object.assign(document.createElement('a'),
-                    {
-                        href: `./community.php?community=${encodeURIComponent(element['name'])}`,
-                        innerText: element['name']
-                    }));
+
+            response.forEach(element => {
+                const type = element['type'];
+                const resultDiv = Object.assign(document.createElement("div"), {className: 'result-'+type});
+                const resultImg = Object.assign(document.createElement("img"), {className: 'svg', src: './res/svg/'+type+'.svg', alt: type});
+                const resultName = Object.assign(document.createElement("a"), {
+                    href: `./${type}.php?${type=='profile'?'username':'community'}=${encodeURIComponent(element['name'])}`,
+                    innerText: element['name']
+                });
+
+                resultDiv.appendChild(resultImg);
+                resultDiv.appendChild(resultName);
+                searchSpace.appendChild(resultDiv);
             });
         }, { value: searchBar.value })
     }
--- site/requests/request-search.php
+++ site/requests/request-search.php
@@ -4,11 +4,9 @@ include_once '../bootstrap.php';
 $args = json_decode($_POST['args'], false);
 
 if (!isset($args->value)) {
-    echo json_encode("error");
+    echo json_encode(false);
     exit();
 }
-$user = $dbh->searchUser($args->value);
-$communities = $dbh->searchCommunities($args->value);
-echo json_encode(array('user'=>$user, 'communities'=>$communities));
 
+echo json_encode($dbh->searchUsersAndCommunities($args->value));
 ?>
