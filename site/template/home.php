3aaa342ef5897a9854dcad181163c8b32107bdde
Emanuele Borghini <manuborg2002@gmail.com>
2023-01-05 20:16:58 +0100

    Write better button values for home Communities and Users buttons

--- site/template/home.php
+++ site/template/home.php
@@ -1,5 +1,5 @@
 <div class='buttons'>
-    <button id='communitiesPosts'>Communities</button>
-    <button id='usersPosts' <?php if (!$isUserLogged) echo 'disabled'; ?>>Users</button>
+    <button id='communitiesPosts'><?php if (!$isUserLogged) echo 'Random '; ?>Communities posts</button>
+    <button id='usersPosts' <?php if (!$isUserLogged) echo 'disabled'; ?>>Users posts</button>
 </div>
 <div id='postsSpace'></div>
