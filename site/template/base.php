6541dbffafb5a08502d997d4cbd2b7e078dfc3c5
Samuele De Tuglie <samuele.de.tuglie@gmail.com>
2023-01-06 04:21:34 +0100

    Improved css and moved checkbox inside the navbar

--- site/template/base.php
+++ site/template/base.php
@@ -12,58 +12,58 @@
 </head>
 
 <body>
-    <input type='checkbox' id='toggle'>
     <nav>
         <h1><a href='./index.php' class='logo'>Reddint</a></h1>
+        <input type='checkbox' id='toggle'>
         <label class='navbar-toggler' for='toggle'>
             <span class='bar'></span>
             <span class='bar'></span>
             <span class='bar'></span>
         </label>
         <ul class='nav-list'>
-            <li class='nav-link nav-item'>
+            <li class='nav-item'>
                 <input type='text' id='search' placeholder='Search'>
                 <div id='searchSpace'></div>
             </li>
-            <li class='nav-link nav-item'>
+            <li class='nav-item'>
                 <a href='community.php'>
                     <img class='svg' alt='communities icon' src='<?php echo RES_SVG;?>community.svg' />
                     Communities
                 </a>
             </li>
             <?php if (!$isUserLogged): ?>
-                <li class='nav-link nav-item'>
+                <li class='nav-item'>
                     <a href='login.php'>
                         <img class='svg' alt='login icon' src='<?php echo RES_SVG;?>login.svg' />
                         Login
                     </a>
                 </li>
-                <li class='nav-link nav-item'>
+                <li class='nav-item'>
                     <a href='signup.php'>
                         <img class='svg' alt='signup icon' src='<?php echo RES_SVG;?>signup.svg' />
                         Signup
                     </a>
                 </li>
             <?php else: ?>
-                <li class='nav-link nav-item'>
+                <li class='nav-item'>
                     <a href='create-post-community.php'>
                         <img class='svg' alt='create icon' src='<?php echo RES_SVG;?>create.svg' />
                         Create
                     </a>
                 </li>
-                <li class='nav-link nav-item'>
+                <li class='nav-item'>
                     <a href=<?php echo 'profile.php?username=' . $_SESSION['username']; ?>>
                         <img class='svg' alt='profile icon' src='<?php echo RES_SVG;?>profile.svg' />
                         Profile
                     </a>
                 </li>
-                <li class='nav-link nav-item'>
+                <li class='nav-item'>
                     <a href='notifications.php'>
                         <img class='svg' alt='notification icon' src='<?php echo RES_SVG;?>notification-bell.svg' />
                         Notifications&nbsp;<?php echo ($isUserLogged ? $templateParams['numNotifications'] : ''); ?>
                     </a>
                 </li>
-                <li class='nav-link nav-item'>
+                <li class='nav-item'>
                     <a href='settings.php'>
                         <img class='svg' alt='settings icon' src='<?php echo RES_SVG;?>settings.svg' />
                         Settings
