16097786cf5f9ca3c9a36030da8ee8ec61319483
Emanuele Borghini <manuborg2002@gmail.com>
2023-01-10 19:11:26 +0100

    Prepare settings page elements for css

--- site/template/settings-page.php
+++ site/template/settings-page.php
@@ -1,38 +1,38 @@
 <section>
-    <h1>Change Username</h1>
-    <form method='POST' id='changeUsername'>
+    <h2>Change Username</h2>
+    <form method='POST' class='settingsForm' id='changeUsername'>
         <input type='text' name='newUsername' placeholder='New Username' value="<?php echo $_SESSION['username']; ?>"
             required /><br />
         <input type='submit' name='submitUsername' value='Change username' /><br />
     </form>
-    <div id='messageUsername'></div>
+    <div class='outputMessageSettings' id='messageUsername'></div>
 </section>
 <section>
-    <h1>Change Bio</h1>
-    <form method='POST' id='changeBio'>
+    <h2>Change Bio</h2>
+    <form method='POST' class='settingsForm' id='changeBio'>
         <textarea maxlength='2048' name='newBio' placeholder='Enter bio'
             required><?php echo $templateParams['userBio']; ?></textarea><br />
         <input type='submit' name='submitBio' value='Change bio' /><br />
     </form>
-    <div id='messageBio'></div>
+    <div class='outputMessageSettings' id='messageBio'></div>
 </section>
 <section>
-    <h1>Change Password</h1>
-    <form method='POST' id='changePassword'>
+    <h2>Change Password</h2>
+    <form method='POST' class='settingsForm' id='changePassword'>
         <input type='password' name='newPassword' placeholder='New Password' required /><br />
         <input type='password' name='confirmNewPassword' placeholder='Confirm New Password' required /><br />
         <input type='submit' name='submitPassword' value='Change password' /><br />
     </form>
-    <div id='messagePassword'></div>
+    <div class='outputMessageSettings' id='messagePassword'></div>
 </section>
 <section>
-    <h1>Logout</h1>
-    <form method='POST' id='logout'>
+    <h2>Logout</h2>
+    <form method='POST' class='settingsForm' id='logout'>
         <input type='submit' name='submitLogout' value='Logout' /><br />
     </form>
 </section>
 <section>
-    <h1>Delete account</h1>
+    <h2>Delete account</h2>
     <div class='deleteButton'>
         <button type='button' id='deleteProfile'>Delete</button>
     </div>
