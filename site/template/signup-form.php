1d92ea9706924e9636321153e9600d8f17658802
Cristina Zoccola <cristina.zoccola01@gmail.com>
2023-01-10 18:11:42 +0100

    Added signup css

--- site/template/signup-form.php
+++ site/template/signup-form.php
@@ -1,11 +1,22 @@
-<form method='POST' id='signup-form'>
-    <h2>Sign up</h2>
-    <ul>
-        <li><label for='email'>Email:</label><input type='email' id='email' name='email' required /></li>
-        <li><label for='username'>Username:</label><input type='text' id='username' name='username' required /></li>
-        <li><label for='password'>Password:</label><input type='password' id='password' name='password' required /></li>
-        <li><input type='submit' name='submit' value='Sign up' /></li>
-    </ul>
-    <p id='response'></p>
-</form>
-<p>Do you already have an account? <a href='./login.php'>Login now!</a></p>
+<div class="signupForm">
+    <div class="signupContainer">
+        <h2>Sign up</h2>
+        <form method='POST' id='signup-form'>
+            <div class="txtField">
+                <label for='email'>Email:</label>
+                <input type='email' id='email' name='email' required />
+            </div>
+            <div class="txtField">
+                <label for='username'>Username:</label>
+                <input type='text' id='username' name='username' required />
+            </div>
+            <div class="txtField">
+                <label for='password'>Password:</label>
+                <input type='password' id='password' name='password' required />
+            </div>
+            <input type='submit' name='submit' value='Sign up' />
+            <p class='outputSignup' id='response'></p>
+            <p>Do you already have an account? <a href='./login.php'>Login now!</a></p>
+        </form>
+    </div>
+</div>
