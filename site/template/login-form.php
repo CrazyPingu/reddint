032a85b7f9d5c60230ffcac4ca9f6f503252ae9f
Emanuele Borghini <manuborg2002@gmail.com>
2023-01-10 17:10:15 +0100

    Changed login form html to reflect css

--- site/template/login-form.php
+++ site/template/login-form.php
@@ -1,10 +1,18 @@
-<form method='POST' id='login-form'>
-    <h2>Login</h2>
-    <ul>
-        <li><label for='email'>Email: </label><input type='email' id='email' name='email' required /></li>
-        <li><label for='password'>Password: </label><input type='password' id='password' name='password' required /></li>
-        <li><input type='submit' name='submit' value='Login' /></li>
-    </ul>
-    <p id='response'></p>
-</form>
-<p>Do you need an account?<a href='./signup.php'>Sign up now!</a></p>
+<div class="loginForm">
+    <div class='loginContainer'>
+        <h2>Login</h2>
+        <form method='POST' id='login-form'>
+            <div class='txtField'>
+                <label for='email'>Email:</label>
+                <input type='email' id='email' name='email' required />
+            </div>
+            <div class='txtField'>
+                <label for='password'>Password:</label>
+                <input type='password' id='password' name='password' required />
+            </div>
+            <input type='submit' name='submit' value='Login' />
+            <p class='outputLogin' id='response'></p>
+            <p>Do you need an account?&nbsp;<a href='./signup.php'>Sign up now!</a></p>
+        </form>
+    </div>
+</div>
