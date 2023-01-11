4cdf15e4183252fd1e3e905be5f96f5a325b1cfe
Emanuele Borghini <manuborg2002@gmail.com>
2023-01-11 19:17:56 +0100

    Add throttle function to limit the server requests

---
+++ site/js/throttle.js
@@ -0,0 +1,10 @@
+// This function is used to throttle server requests
+var throttleTimer;
+export const throttle = (callback, time) => {
+    if (throttleTimer) return;
+    throttleTimer = true;
+    setTimeout(() => {
+        callback();
+        throttleTimer = false;
+    }, time);
+};
