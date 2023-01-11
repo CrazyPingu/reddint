2cec224a1fca8e5aae793923e4416e270ce84cf8
Cristina Zoccola <cristina.zoccola01@gmail.com>
2023-01-11 11:23:40 +0100

    Added css for the creation forms

--- site/template/create-form.php
+++ site/template/create-form.php
@@ -1,7 +1,7 @@
-<?php if (!count($templateParams['communities']) == 0): ?>
+<?php if (count($templateParams['communities']) != 0): ?>
     <section>
         <h2>Create new post</h2>
-        <form method='post' id='createPost'>
+        <form method='post' class='createForm' id='createPost'>
             <select name='community' id='communityList'>
                 <?php foreach ($templateParams['communities'] as $community): ?>
                     <option value='<?php echo $community['name'] ?>'>
@@ -17,7 +17,7 @@
 <?php endif; ?>
 <section>
     <h2>Create new community</h2>
-    <form method='post' id='createCommunity'>
+    <form method='post' class='createForm' id='createCommunity'>
         <textarea name='nameCommunity' id='nameCommunity' placeholder='Name' maxlength='64' required></textarea><br />
         <textarea name='description' id='descriptionCommunity' placeholder='Description' maxlength='2048'
             required></textarea><br />
